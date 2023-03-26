7052d9f0c960ea86de5b6239a562618f7f377113
nasimjon97 <nasimjon078@gmail.com>
2023-03-27 00:44:06 +0500

    sending a letter to the post office

--- app/Http/Controllers/QueueCabinetController.php
+++ app/Http/Controllers/QueueCabinetController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ReserveCabinetRequest;
+use App\Mail\SendMail;
 use App\Models\Cabinet;
 use App\Models\QueueCabinet;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class QueueCabinetController extends Controller
 {
@@ -30,23 +32,26 @@ class QueueCabinetController extends Controller
     public function reserve_cabinet(ReserveCabinetRequest $request)
     {
         $user = User::updateOrCreate([
+            'email' => $request->email,
+        ],[
             'name' => $request->name,
             'email' => $request->email,
             'phone' => $request->phone,
-        ],[
-            'email' => $request->email,
         ]);
         $request['user_id'] = $user->id;
         $from_date_time = $request->from_date_time;
         $to_date_time = $request->to_date_time;
-        $queue_cabinet = QueueCabinet::where('cabinet_id',$request->cabinet_id)
-            ->where(function($query) use ($from_date_time,$to_date_time) {
+        $cabinet_id = $request->cabinet_id;
+        $queue_cabinet = QueueCabinet::
+            where(function($query) use ($from_date_time,$to_date_time,$cabinet_id) {
                 $query->where('from', '>=', $from_date_time)
-                    ->where('from', '<=', $to_date_time);
+                    ->where('from', '<=', $to_date_time)
+                    ->where('cabinet_id',$cabinet_id);
                     })
-            ->orWhere(function($query) use ($from_date_time,$to_date_time) {
+            ->orWhere(function($query) use ($from_date_time,$to_date_time,$cabinet_id) {
                 $query->where('to', '>=', $from_date_time)
-                    ->where('to', '<=', $to_date_time);
+                    ->where('to', '<=', $to_date_time)
+                    ->where('cabinet_id',$cabinet_id);
                     })
            ->first();
         if ($queue_cabinet){
@@ -55,12 +60,22 @@ class QueueCabinetController extends Controller
             $to_datetime = Carbon::parse($queue_cabinet->to)->format('Y-m-d H:i');
             return redirect()->route('home')->withInput()->with(['msg' => "Кабинет занят со стороны $user_name от $from_datetime до $to_datetime", 'class' => 'danger']);
         }
-         QueueCabinet::create([
+         $queue_cabinet =QueueCabinet::create([
              'cabinet_id' => $request->cabinet_id,
              'user_id' => $user->id,
              'from' => $request->from_date_time,
              'to' => $request->to_date_time,
          ]);
-        return redirect()->route('home')->withInput()->with(['msg' => 'Кабинет успешно бронирован', 'class' => 'success']);
+        $toEmail = "получатель@yandex.ru";
+
+
+        // Generate message for mail
+        $from_datetime = Carbon::parse($queue_cabinet->from)->format('Y-m-d H:i');
+        $to_datetime = Carbon::parse($queue_cabinet->to)->format('Y-m-d H:i');
+        $cabinet = $queue_cabinet->cabinet->name;
+        $message = "$cabinet успешно бронирован от $from_datetime до $to_datetime";
+        //Send message to mail
+        Mail::to($toEmail)->send(new SendMail($message));
+        return redirect()->route('home')->with(['msg' => "Кабинет успешно бронирован", 'class' => 'success']);
     }
 }
